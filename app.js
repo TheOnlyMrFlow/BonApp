2ea7b031afa2e32a0aa1193b2ad39df84d6b2bd3
TheOnlyMrFlow <comte.florian@gmail.com>
2019-04-11 14:52:23 +0200

    gitignore

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
 
+const morgan = require('morgan');
+
 const cors = require("cors");
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+
+'mongodb://MrFlow:' + process.env.MONGO_ATLAS_PASSWORD + '@code-shirts-shard-00-00-s2vos.mongodb.net:27017,code-shirts-shard-00-01-s2vos.mongodb.net:27017,code-shirts-shard-00-02-s2vos.mongodb.net:27017/test?ssl=true&replicaSet=code-shirts-shard-0&authSource=admin'
+
+
+'mongodb://mrflow:' + process.env.MONGO_ATLAS_PASSWORD + '@cluster0-4iqxv.mongodb.net/test?retryWrites=true'
 
 const routes = require('./Routes/routes');
 
 
+app.use(morgan('dev'));
 
 app.use(cors());
 
