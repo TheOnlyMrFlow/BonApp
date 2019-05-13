409f78e2aa0dc5f0166299d0da01dc250798b1df
TheOnlyMrFlow <comte.florian@gmail.com>
2019-05-13 11:50:50 +0200

    patch niveau

--- Routes/Templates/Niveaux/routes.js
+++ Routes/Templates/Niveaux/routes.js
@@ -7,7 +7,7 @@ const niveauxControllers = require('../../../Controllers/niveaux');
 
 router.get('/', niveauxControllers.getNiveauxOfTemplate);
 router.post('/', niveauxControllers.postNiveauInTemplate);
-router.patch('/', niveauxControllers.patchNiveau);
+router.patch('/:niveauId', niveauxControllers.patchNiveau);
 
 
 module.exports = router;
